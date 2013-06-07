34a2c2ae63912e3fd36946dcab9d275eb27b24cb
Adam <achesak@yahoo.com>
2013-06-07 14:28:08 -0500

    Added checks to make sure the data was of the correct type; created more helpful error dialog.

--- weather_or_not.py
+++ weather_or_not.py
@@ -45,6 +45,8 @@ from collections import Counter
 import webbrowser
 # Import datetime for getting the difference between two dates.
 import datetime
+# Import re for validating the data.
+import re
 # Import os for creating a directory.
 import os
 # Import os.path for seeing if a directory exists.
@@ -242,11 +244,69 @@ class Weather(Gtk.Window):
             note = new_dlg.note_ent.get_text()
             
             # If anything required was missing, cancel this action. Everything is required except for the notes.
-            if not date or not temp or not prec or not prec_type or not wind or not wind_dir or not humi or not airp or not clou:
+            # Also check to make sure everything is of the correct type.
+            missing_msg = ""
+            
+            # If the date is missing or of the wrong type:
+            if not date:
+                missing_msg += "Date is missing.\n"
+            elif not re.compile("^\d{1,2}/\d{1,2}/\d{2}$").match(date):
+                missing_msg += "Date does not match the pattern DD/MM/YY.\n"
+            
+            # If the temperature is missing or of the wrong type:
+            if not temp:
+                missing_msg += "Temperature is missing.\n"
+            elif not re.compile("^\d+$").match(temp):
+                missing_msg += "Temperature must be a number.\n"
+            
+            # If the precipitation amount is missing or of the wrong type:
+            if not prec:
+                missing_msg += "Precipitation amount is missing.\n"
+            elif not re.compile("^\d+$").match(prec):
+                missing_msg += "Precipitation amount must be a number.\n"
+            
+            # If the precipitation type is missing or not a valid value:
+            if not prec_type:
+                missing_msg += "Precipitation type is missing.\n"
+            elif prec_type not in ["Rain", "Snow", "Hail", "Sleet"]:
+                missing_msg += "Precipitation type is not a valid value. (This should never happen.)\n"
+            
+            # If the wind speed is missing or of the wrong type:
+            if not wind:
+                missing_msg += "Wind speed is missing.\n"
+            elif not re.compile("^\d+$").match(wind):
+                missing_msg += "Wind speed must be a number.\n"
+            
+            # If the wind direction is missing or not a valid value:
+            if not wind_dir:
+                missing_msg += "Wind direction is missing.\n"
+            elif wind_dir not in ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]:
+                missing_msg += "Wind direction is not a valid value. (This should never happen.)\n"
+            
+            # If the humidity is missing or of the wrong type:
+            if not humi:
+                missing_msg += "Humidity is missing.\n"
+            elif not re.compile("^\d+$").match(humi):
+                missing_msg += "Humidity must be a number.\n"
+            
+            # If the air pressure is missing or of the wrong type:
+            if not airp:
+                missing_msg += "Air pressure is missing.\n"
+            elif not re.compile("^\d+$").match(airp):
+                missing_msg += "Air pressure must be a number.\n"
+            
+            # If the cloud cover is missing or not a valid value:
+            if not clou:
+                missing_msg += "Cloud cover is missing.\n"
+            elif clou not in ["Sunny", "Mostly Sunny", "Partly Cloudy", "Mostly Cloudy", "Cloudy"]:
+                missing_msg += "Cloud cover is not a valud value. (This should never happen.)\n"
+            
+            # If the error message isn't blank, show the dialog.
+            if missing_msg:
                 
                 # Create the error dialog.
                 err_miss_dlg = Gtk.MessageDialog(new_dlg, 0, Gtk.MessageType.WARNING, Gtk.ButtonsType.OK, "Add New")
-                err_miss_dlg.format_secondary_text("One or more fields was left blank.")
+                err_miss_dlg.format_secondary_text("There were one or more problems with the data entered.\n\n%s" % missing_msg.rstrip())
                 
                 # Show the error dialog.
                 err_miss_dlg.run()
