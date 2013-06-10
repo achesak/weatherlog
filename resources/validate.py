c1fd69a5ca786a69d87a064f559170fe90ec24aa
Adam <achesak@yahoo.com>
2013-06-10 10:49:37 -0500

    validate.validate() will now allow numbers that have decimal points.

--- resources/validate.py
+++ resources/validate.py
@@ -23,43 +23,43 @@ def validate(date, temp, prec, prec_type, wind, wind_dir, humi, airp, clou):
     # If the temperature is missing or of the wrong type:
     if not temp:
         missing_msg += "Temperature is missing.\n"
-    elif not re.compile("^\d+$").match(temp):
+    elif not re.compile("^\d+(\.\d+)?$").match(temp):
         missing_msg += "Temperature must be a number.\n"
     
     # If the precipitation amount is missing or of the wrong type:
     if not prec:
         missing_msg += "Precipitation amount is missing.\n"
-    elif not re.compile("^\d+$").match(prec):
+    elif not re.compile("^\d+(\.\d+)?$").match(prec):
         missing_msg += "Precipitation amount must be a number.\n"
     
     # If the precipitation type is missing or not a valid value:
     if not prec_type:
         missing_msg += "Precipitation type is missing.\n"
-    elif prec_type not in ["Rain", "Snow", "Hail", "Sleet"]:
+    elif prec_type not in ["None", "Rain", "Snow", "Hail", "Sleet"]:
         missing_msg += "Precipitation type is not a valid value. (This should never happen.)\n"
     
     # If the wind speed is missing or of the wrong type:
     if not wind:
         missing_msg += "Wind speed is missing.\n"
-    elif not re.compile("^\d+$").match(wind):
+    elif not re.compile("^\d+(\.\d+)?$").match(wind):
         missing_msg += "Wind speed must be a number.\n"
     
     # If the wind direction is missing or not a valid value:
     if not wind_dir:
         missing_msg += "Wind direction is missing.\n"
-    elif wind_dir not in ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]:
+    elif wind_dir not in ["None", "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]:
         missing_msg += "Wind direction is not a valid value. (This should never happen.)\n"
     
     # If the humidity is missing or of the wrong type:
     if not humi:
         missing_msg += "Humidity is missing.\n"
-    elif not re.compile("^\d+$").match(humi):
+    elif not re.compile("^\d+(\.\d+)?$").match(humi):
         missing_msg += "Humidity must be a number.\n"
     
     # If the air pressure is missing or of the wrong type:
     if not airp:
         missing_msg += "Air pressure is missing.\n"
-    elif not re.compile("^\d+$").match(airp):
+    elif not re.compile("^\d+(\.\d+)?$").match(airp):
         missing_msg += "Air pressure must be a number.\n"
     
     # If the cloud cover is missing or not a valid value:
