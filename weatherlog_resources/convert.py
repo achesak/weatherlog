4722e1e43efd1540d7d7752ef608a1cd5aea9918
achesak <achesak@yahoo.com>
2014-02-16 16:47:33 -0600

    Rewrote convert.convert() to be faster and simpler.

--- weatherlog_resources/convert.py
+++ weatherlog_resources/convert.py
@@ -14,47 +14,18 @@ def convert(data2, units):
     # Make a copy of the data. The original list should remain unchanged.
     data = data2[:]
     
-    # Convert the temperature.
-    # Metric to imperial:
-    if units == "imperial":
+    # Loop through the list and convert as needed.
+    for i in range(0, len(data)):
         
-        for i in range(0, len(data)):
+        # Convert from imperial to metric:
+        if units == "metric":
             
-            # Celsius to Fahrenheit:
-            # F = 9/5C + 32
-            data[i][1] = "%.2f" % ((float(data[i][1]) * (9 / 5)) + 32)
-    
-    # Imperial to metric:
-    elif units == "metric":
-        
-        for i in range(0, len(data)):
-            
+            # Convert the temperature.
             # Fahrenheit to Celsius:
             # C = 5/9(F + 32)
             data[i][1] = "%.2f" % ((float(data[i][1]) - 32) * (5 / 9))
-    
-    # Convert the precipitation.
-    # Metric to imperial:
-    if units == "imperial":
-        
-        for i in range(0, len(data)):
             
-            # Centimeters to inches:
-            # in = cm / 2.54
-            # Skip "None" values.
-            if data[i][2] != "None":
-                
-                split = data[i][2].split(" ")
-            
-                split[0] = "%.2f" % (float(split[0]) / 2.54)
-                
-                data[i][2] = " ".join(split)
-    
-    # Imperial to metric:
-    elif units == "metric":
-        
-        for i in range(0, len(data)):
-            
+            # Convert the precipitation amount.
             # Inches to centimeters:
             # cm = in * 2.54
             # Skip "None" values.
@@ -65,29 +36,8 @@ def convert(data2, units):
                 split[0] = "%.2f" % (float(split[0]) * 2.54)
                 
                 data[i][2] = " ".join(split)
-    
-    # Convert the wind.
-    # Metric to imperial:
-    if units == "imperial":
-        
-        for i in range(0, len(data)):
             
-            # Kilometers per hour to miles per hour:
-            # mph = kph / 1.60934
-            # Skip "None" values.
-            if data[i][3] != "None":
-                
-                split = data[i][3].split(" ")
-            
-                split[0] = "%.2f" % (float(split[0]) / 1.60934)
-                
-                data[i][3] = " ".join(split)
-    
-    # Imperial to metric:
-    elif units == "metric":
-        
-        for i in range(0, len(data)):
-            
+            # Convert the wind speed.
             # Miles per hour to kilometers per hour:
             # kph = mph * 1.60934
             # Skip "None" values.
@@ -96,6 +46,38 @@ def convert(data2, units):
                 split = data[i][3].split(" ")
             
                 split[0] = "%.2f" % (float(split[0]) * 1.60934)
+                
+                data[i][3] = " ".join(split)
+        
+        # Convert from metric to imperial:
+        elif units == "imperial":
+            
+            # Convert the temperature.
+            # Celsius to Fahrenheit:
+            # F = 9/5C + 32
+            data[i][1] = "%.2f" % ((float(data[i][1]) * (9 / 5)) + 32)
+            
+            # Convert the precipitation amount.
+            # Centimeters to inches:
+            # in = cm / 2.54
+            # Skip "None" values.
+            if data[i][2] != "None":
+                
+                split = data[i][2].split(" ")
+            
+                split[0] = "%.2f" % (float(split[0]) / 2.54)
+                
+                data[i][2] = " ".join(split)
+            
+            # Convert the wind speed.
+            # Kilometers per hour to miles per hour:
+            # mph = kph / 1.60934
+            # Skip "None" values.
+            if data[i][3] != "None":
+                
+                split = data[i][3].split(" ")
+            
+                split[0] = "%.2f" % (float(split[0]) / 1.60934)
                 
                 data[i][3] = " ".join(split)
     
