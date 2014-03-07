243dbf25af256252a5f400cd1777dacdebec38a0
achesak <achesak@yahoo.com>
2014-03-07 13:51:07 -0600

    Bumped version number.

--- weatherlog.py
+++ weatherlog.py
@@ -5,7 +5,7 @@
 ################################################################################
 
 # WeatherLog
-# Version 1.6
+# Version 1.7
 
 # WeatherLog is an application for keeping track of the weather and
 # getting information about past trends.
