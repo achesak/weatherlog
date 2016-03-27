6a6b297e25490739f739a08c656f5280d7a6214d
achesak <achesak@yahoo.com>
2016-03-27 16:13:18 -0500

    Misc dialogs now use application constants.

--- weatherlog_resources/dialogs/misc_dialogs.py
+++ weatherlog_resources/dialogs/misc_dialogs.py
@@ -6,6 +6,8 @@
 
 # Import GTK for the dialog.
 from gi.repository import Gtk
+# Import the application constants.
+from weatherlog_resources.constants import *
 
 __all__ = ["show_alert_dialog", "show_error_dialog", "show_question_dialog", "show_file_dialog",
            "show_export_dialog", "show_save_dialog", "show_no_data_dialog"]
@@ -42,10 +44,7 @@ def show_question_dialog(self, title, msg):
 def show_file_dialog(self, title):
     """Shows the file chooser (open) dialog."""
     
-    # Create the dialog.
     import_dlg = Gtk.FileChooserDialog(title, self, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-    
-    # Get the response and filename then close the dialog.
     response = import_dlg.run()
     filename = import_dlg.get_filename()
     import_dlg.destroy()
@@ -55,11 +54,8 @@ def show_file_dialog(self, title):
 def show_export_dialog(self, title):
     """Shows the file chooser (open) dialog."""
     
-    # Create the dialog.
     export_dlg = Gtk.FileChooserDialog(title, self, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Export", Gtk.ResponseType.OK))
     export_dlg.set_do_overwrite_confirmation(True)
-    
-    # Get the response and filename then close the dialog.
     response = export_dlg.run()
     filename = export_dlg.get_filename()
     export_dlg.destroy()
@@ -69,11 +65,9 @@ def show_export_dialog(self, title):
 def show_save_dialog(self, title):
     """Shows the file chooser (save) dialog."""
     
-    # Create the dialog.
-    export_dlg = Gtk.FileChooserDialog(title, self, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Export to CSV", 98, "Export to HTML", 99, "Export", Gtk.ResponseType.OK))
+    export_dlg = Gtk.FileChooserDialog(title, self, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Export to CSV", DialogResponse.EXPORT_CSV,
+                                       "Export to HTML", DialogResponse.EXPORT_HTML, "Export", Gtk.ResponseType.OK))
     export_dlg.set_do_overwrite_confirmation(True)
-    
-    # Get the response and filename then close the dialog.
     response = export_dlg.run()
     filename = export_dlg.get_filename()
     export_dlg.destroy()
