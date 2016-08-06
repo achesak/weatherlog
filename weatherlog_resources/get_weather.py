d8bfa351fac53c0e306eda1ceb4337e9c1ae47d3
achesak <achesak@yahoo.com>
2016-08-06 17:57:47 -0500

    Current Weather now rounds to one decimal instead of none; this fixes '0 cm' rain and the like appearing.

--- weatherlog_resources/get_weather.py
+++ weatherlog_resources/get_weather.py
@@ -52,13 +52,13 @@ def get_weather(config, units, weather_codes, location, location_type):
         conditions.append(weather_codes[int(cond["id"])])
     data1 = [
         ["Condition", "\n".join(conditions)],
-        ["Temperature", "%d %s" % (result["main"]["temp"], units["temp"])],
-        ["Temperature (minimum)", "%d %s" % (result["main"]["temp_min"], units["temp"])],
-        ["Temperature (maximum)", "%d %s" % (result["main"]["temp_max"], units["temp"])],
-        ["Wind speed", "%s %s" % (result["wind"]["speed"], units["wind"])],
+        ["Temperature", "%.1f %s" % (result["main"]["temp"], units["temp"])],
+        ["Temperature (minimum)", "%.1f %s" % (result["main"]["temp_min"], units["temp"])],
+        ["Temperature (maximum)", "%.1f %s" % (result["main"]["temp_max"], units["temp"])],
+        ["Wind speed", "%.1f %s" % (result["wind"]["speed"], units["wind"])],
         ["Wind direction", degrees.degree_to_direction(int(result["wind"]["deg"]))],
         ["Humidity", "%d%%" % result["main"]["humidity"]],
-        ["Air pressure", "%d %s" % (result["main"]["pressure"], units["airp"])],
+        ["Air pressure", "%.1f %s" % (result["main"]["pressure"], units["airp"])],
         ["Cloud cover", ["Sunny", "Mostly sunny", "Partly cloudy", "Mostly cloudy", "Cloudy"][clouds.percent_to_term(result["clouds"]["all"])]],
         ["Sunrise", datetime.datetime.fromtimestamp(result["sys"]["sunrise"]).strftime("%H:%M:%S")],
         ["Sunset", datetime.datetime.fromtimestamp(result["sys"]["sunset"]).strftime("%H:%M:%S")]
@@ -78,21 +78,21 @@ def get_weather(config, units, weather_codes, location, location_type):
             conditions.append(weather_codes[int(cond["id"])])
         data3.append(["Date", datetime.datetime.fromtimestamp(fc["dt"]).strftime("%d/%m/%Y")])
         data3.append(["Condition", "\n".join(conditions)])
-        data3.append(["Temperature", "%d %s" % (fc["temp"]["day"], units["temp"])])
-        data3.append(["Temperature (minimum)", "%d %s" % (fc["temp"]["min"], units["temp"])])
-        data3.append(["Temperature (maximum)", "%d %s" % (fc["temp"]["max"], units["temp"])])
-        data3.append(["Temperature (morning)", "%d %s" % (fc["temp"]["morn"], units["temp"])])
-        data3.append(["Temperature (evening)", "%d %s" % (fc["temp"]["eve"], units["temp"])])
-        data3.append(["Temperature (night)", "%d %s" % (fc["temp"]["night"], units["temp"])])
+        data3.append(["Temperature", "%.1f %s" % (fc["temp"]["day"], units["temp"])])
+        data3.append(["Temperature (minimum)", "%.1f %s" % (fc["temp"]["min"], units["temp"])])
+        data3.append(["Temperature (maximum)", "%.1f %s" % (fc["temp"]["max"], units["temp"])])
+        data3.append(["Temperature (morning)", "%.1f %s" % (fc["temp"]["morn"], units["temp"])])
+        data3.append(["Temperature (evening)", "%.1f %s" % (fc["temp"]["eve"], units["temp"])])
+        data3.append(["Temperature (night)", "%.1f %s" % (fc["temp"]["night"], units["temp"])])
         data3.append(["Wind speed", "%s %s" % (fc["speed"], units["wind"])])
         data3.append(["Wind direction", degrees.degree_to_direction(int(fc["deg"]))])
         data3.append(["Humidity", "%d%%" % fc["humidity"]])
-        data3.append(["Air pressure", "%d %s" % (fc["pressure"], units["airp"])])
+        data3.append(["Air pressure", "%.1f %s" % (fc["pressure"], units["airp"])])
         data3.append(["Cloud cover", ["Sunny", "Mostly sunny", "Partly cloudy", "Mostly cloudy", "Cloudy"][clouds.percent_to_term(fc["clouds"])]])
         if "rain" in fc:
-            data3.append(["Precipitation (rain)", "%d %s" % (fc["rain"], units["prec"])])
+            data3.append(["Precipitation (rain)", "%.1f %s" % (fc["rain"], units["prec"])])
         if "snow" in fc:
-            data3.append(["Precipitation (snow)", "%d %s" % (fc["snow"], units["prec"])])
+            data3.append(["Precipitation (snow)", "%.1f %s" % (fc["snow"], units["prec"])])
         if i != len(forecast["list"]) - 1:
             data3.append(["", ""])
 
