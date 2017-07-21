7691c17bd2d4594e108dfc90bfe8129b95f23bb2
achesak <achesak@yahoo.com>
2017-07-20 20:38:55 -0500

    Fix PEP8 warnings

--- weatherlog_resources/dialogs/edit_dialog.py
+++ weatherlog_resources/dialogs/edit_dialog.py
@@ -14,41 +14,40 @@ from gi.repository import Gtk
 
 # Import application modules.
 import weatherlog_resources.datasets as datasets
-from weatherlog_resources.dialogs.misc_dialogs import *
 
 
 class EditDialog(Gtk.Dialog):
     """Shows the "Edit" dialog."""
-    
+
     def __init__(self, parent, dataset, data, date, units):
         """Create the dialog."""
-        
+
         # Determine the default units.
         unit = 0
         if units["wind"] == "mph":
             unit = 1
-        
+
         Gtk.Dialog.__init__(self, "Edit %s - %s" % (date, dataset), parent, Gtk.DialogFlags.MODAL)
         self.set_size_request(500, 600)
         self.add_button("Cancel", Gtk.ResponseType.CANCEL)
         self.add_button("OK", Gtk.ResponseType.OK)
-        
+
         # Create the grids.
         new_box = self.get_content_area()
         new_grid = Gtk.Grid()
         new_grid.set_column_spacing(3)
         new_grid.set_row_spacing(3)
         new_box.add(new_grid)
-        
+
         # Temperature entry
         temp_lbl = Gtk.Label("Temperature: ")
         temp_lbl.set_alignment(0, 0.5)
         new_grid.add(temp_lbl)
         if units["temp"].encode("utf-8") == "°C":
-            temp_adj = Gtk.Adjustment(lower = -100, upper = 100, step_increment = 1)
+            temp_adj = Gtk.Adjustment(lower=-100, upper=100, step_increment=1)
         else:
-            temp_adj = Gtk.Adjustment(lower = -150, upper = 150, step_increment = 1)
-        self.temp_sbtn = Gtk.SpinButton(digits = 2, adjustment = temp_adj)
+            temp_adj = Gtk.Adjustment(lower=-150, upper=150, step_increment=1)
+        self.temp_sbtn = Gtk.SpinButton(digits=2, adjustment=temp_adj)
         self.temp_sbtn.set_numeric(False)
         self.temp_sbtn.set_value(0)
         new_grid.attach_next_to(self.temp_sbtn, temp_lbl, Gtk.PositionType.RIGHT, 1, 1)
@@ -57,16 +56,16 @@ class EditDialog(Gtk.Dialog):
             self.temp_unit.append_text(i)
         self.temp_unit.set_active(unit)
         new_grid.attach_next_to(self.temp_unit, self.temp_sbtn, Gtk.PositionType.RIGHT, 1, 1)
-        
+
         # Wind Chill entry
         chil_lbl = Gtk.Label("Wind Chill: ")
         chil_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(chil_lbl, temp_lbl, Gtk.PositionType.BOTTOM, 1, 1)
         if units["temp"].encode("utf-8") == "°C":
-            chil_adj = Gtk.Adjustment(lower = -100, upper = 100, step_increment = 1)
+            chil_adj = Gtk.Adjustment(lower=-100, upper=100, step_increment=1)
         else:
-            chil_adj = Gtk.Adjustment(lower = -150, upper = 150, step_increment = 1)
-        self.chil_sbtn = Gtk.SpinButton(digits = 2, adjustment = chil_adj)
+            chil_adj = Gtk.Adjustment(lower=-150, upper=150, step_increment=1)
+        self.chil_sbtn = Gtk.SpinButton(digits=2, adjustment=chil_adj)
         self.chil_sbtn.set_numeric(False)
         self.chil_sbtn.set_value(0)
         new_grid.attach_next_to(self.chil_sbtn, chil_lbl, Gtk.PositionType.RIGHT, 1, 1)
@@ -75,13 +74,13 @@ class EditDialog(Gtk.Dialog):
             self.chil_unit.append_text(i)
         self.chil_unit.set_active(unit)
         new_grid.attach_next_to(self.chil_unit, self.chil_sbtn, Gtk.PositionType.RIGHT, 1, 1)
-        
+
         # Precipitation entry
         prec_lbl = Gtk.Label("Precipitation: ")
         prec_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(prec_lbl, chil_lbl, Gtk.PositionType.BOTTOM, 1, 1)
-        prec_adj = Gtk.Adjustment(lower = 0, upper = 100, step_increment = 1)
-        self.prec_sbtn = Gtk.SpinButton(digits = 2, adjustment = prec_adj)
+        prec_adj = Gtk.Adjustment(lower=0, upper=100, step_increment=1)
+        self.prec_sbtn = Gtk.SpinButton(digits=2, adjustment=prec_adj)
         self.prec_sbtn.set_numeric(False)
         self.prec_sbtn.set_value(0)
         new_grid.attach_next_to(self.prec_sbtn, prec_lbl, Gtk.PositionType.RIGHT, 1, 1)
@@ -90,7 +89,7 @@ class EditDialog(Gtk.Dialog):
             self.prec_unit.append_text(i)
         self.prec_unit.set_active(unit)
         new_grid.attach_next_to(self.prec_unit, self.prec_sbtn, Gtk.PositionType.RIGHT, 1, 1)
-        
+
         # Precipitation Type entry
         prec_lbl2 = Gtk.Label("Precipitation Type: ")
         prec_lbl2.set_alignment(0, 0.5)
@@ -100,13 +99,13 @@ class EditDialog(Gtk.Dialog):
             self.prec_com.append_text(i)
         self.prec_com.set_active(0)
         new_grid.attach_next_to(self.prec_com, prec_lbl2, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Wind Speed entry
         wind_lbl = Gtk.Label("Wind Speed: ")
         wind_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(wind_lbl, prec_lbl2, Gtk.PositionType.BOTTOM, 1, 1)
-        wind_adj = Gtk.Adjustment(lower = 0, upper = 500, step_increment = 1)
-        self.wind_sbtn = Gtk.SpinButton(digits = 2, adjustment = wind_adj)
+        wind_adj = Gtk.Adjustment(lower=0, upper=500, step_increment=1)
+        self.wind_sbtn = Gtk.SpinButton(digits=2, adjustment=wind_adj)
         self.wind_sbtn.set_numeric(False)
         self.wind_sbtn.set_value(0)
         new_grid.attach_next_to(self.wind_sbtn, wind_lbl, Gtk.PositionType.RIGHT, 1, 1)
@@ -115,33 +114,34 @@ class EditDialog(Gtk.Dialog):
             self.wind_unit.append_text(i)
         self.wind_unit.set_active(unit)
         new_grid.attach_next_to(self.wind_unit, self.wind_sbtn, Gtk.PositionType.RIGHT, 1, 1)
-        
+
         # Wind Direction entry
         wind_lbl2 = Gtk.Label("Wind Direction: ")
         wind_lbl2.set_alignment(0, 0.5)
         new_grid.attach_next_to(wind_lbl2, wind_lbl, Gtk.PositionType.BOTTOM, 1, 1)
         self.wind_com = Gtk.ComboBoxText()
-        for i in ["None", "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]:
+        for i in ["None", "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW",
+                  "NNW"]:
             self.wind_com.append_text(i)
         self.wind_com.set_active(0)
         new_grid.attach_next_to(self.wind_com, wind_lbl2, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Humidity entry
         humi_lbl = Gtk.Label("Humidity %: ")
         humi_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(humi_lbl, wind_lbl2, Gtk.PositionType.BOTTOM, 1, 1)
-        humi_adj = Gtk.Adjustment(lower = 0, upper = 100, step_increment = 1)
-        self.humi_sbtn = Gtk.SpinButton(digits = 2, adjustment = humi_adj)
+        humi_adj = Gtk.Adjustment(lower=0, upper=100, step_increment=1)
+        self.humi_sbtn = Gtk.SpinButton(digits=2, adjustment=humi_adj)
         self.humi_sbtn.set_numeric(False)
         self.humi_sbtn.set_value(0)
         new_grid.attach_next_to(self.humi_sbtn, humi_lbl, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Air Pressure entry
         airp_lbl = Gtk.Label("Air Pressure: ")
         airp_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(airp_lbl, humi_lbl, Gtk.PositionType.BOTTOM, 1, 1)
-        airp_adj = Gtk.Adjustment(lower = 0, upper = 2000, step_increment = 1)
-        self.airp_sbtn = Gtk.SpinButton(digits = 2, adjustment = airp_adj)
+        airp_adj = Gtk.Adjustment(lower=0, upper=2000, step_increment=1)
+        self.airp_sbtn = Gtk.SpinButton(digits=2, adjustment=airp_adj)
         self.airp_sbtn.set_numeric(False)
         self.airp_sbtn.set_value(0)
         new_grid.attach_next_to(self.airp_sbtn, airp_lbl, Gtk.PositionType.RIGHT, 1, 1)
@@ -150,7 +150,7 @@ class EditDialog(Gtk.Dialog):
             self.airp_unit.append_text(i)
         self.airp_unit.set_active(unit)
         new_grid.attach_next_to(self.airp_unit, self.airp_sbtn, Gtk.PositionType.RIGHT, 1, 1)
-        
+
         # Air Pressure Change entry
         airp_lbl2 = Gtk.Label("Air Pressure Change: ")
         airp_lbl2.set_alignment(0, 0.5)
@@ -160,13 +160,13 @@ class EditDialog(Gtk.Dialog):
             self.airp_com.append_text(i)
         self.airp_com.set_active(0)
         new_grid.attach_next_to(self.airp_com, airp_lbl2, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Visibility entry
         visi_lbl = Gtk.Label("Visibility: ")
         visi_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(visi_lbl, airp_lbl2, Gtk.PositionType.BOTTOM, 1, 1)
-        visi_adj = Gtk.Adjustment(lower = 0, upper = 1000, step_increment = 1)
-        self.visi_sbtn = Gtk.SpinButton(digits = 2, adjustment = visi_adj)
+        visi_adj = Gtk.Adjustment(lower=0, upper=1000, step_increment=1)
+        self.visi_sbtn = Gtk.SpinButton(digits=2, adjustment=visi_adj)
         self.visi_sbtn.set_numeric(False)
         self.visi_sbtn.set_value(0)
         new_grid.attach_next_to(self.visi_sbtn, visi_lbl, Gtk.PositionType.RIGHT, 1, 1)
@@ -175,7 +175,7 @@ class EditDialog(Gtk.Dialog):
             self.visi_unit.append_text(i)
         self.visi_unit.set_active(unit)
         new_grid.attach_next_to(self.visi_unit, self.visi_sbtn, Gtk.PositionType.RIGHT, 1, 1)
-        
+
         # Cloud Cover entry
         clou_lbl = Gtk.Label("Cloud Cover: ")
         clou_lbl.set_alignment(0, 0.5)
@@ -185,20 +185,21 @@ class EditDialog(Gtk.Dialog):
             self.clou_com.append_text(i)
         self.clou_com.set_active(0)
         new_grid.attach_next_to(self.clou_com, clou_lbl, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Cloud Type entry
         clou_lbl2 = Gtk.Label("Cloud Type: ")
         clou_lbl2.set_alignment(0, 0.5)
         new_grid.attach_next_to(clou_lbl2, clou_lbl, Gtk.PositionType.BOTTOM, 1, 1)
         self.clou_com2 = Gtk.ComboBoxText()
-        for i in ["None", "Unknown", "Cirrus", "Cirrocumulus", "Cirrostratus", "Cumulonimbus", "Altocumulus", "Altostratus",
+        for i in ["None", "Unknown", "Cirrus", "Cirrocumulus", "Cirrostratus", "Cumulonimbus", "Altocumulus",
+                  "Altostratus",
                   "Stratus", "Cumulus", "Stratocumulus"]:
             self.clou_com2.append_text(i)
         self.clou_com2.set_active(0)
         new_grid.attach_next_to(self.clou_com2, clou_lbl2, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Notes entry
-        note_lbl = Gtk.Label("Notes: ", valign = Gtk.Align.START)
+        note_lbl = Gtk.Label("Notes: ", valign=Gtk.Align.START)
         note_lbl.set_alignment(0, 0.5)
         new_grid.attach_next_to(note_lbl, clou_lbl2, Gtk.PositionType.BOTTOM, 1, 1)
         self.note_view = Gtk.TextView()
@@ -210,7 +211,7 @@ class EditDialog(Gtk.Dialog):
         note_win.set_size_request(100, 100)
         note_win.add(self.note_view)
         new_grid.attach_next_to(note_win, note_lbl, Gtk.PositionType.RIGHT, 2, 1)
-        
+
         # Set the values.
         self.temp_sbtn.set_value(float(data[1]))
         self.chil_sbtn.set_value(float(data[2]))
@@ -224,7 +225,8 @@ class EditDialog(Gtk.Dialog):
             self.prec_sbtn.set_value(float(d2[0]))
         if data[4] != "None":
             d3 = data[4].split(" ")
-            wind_list = ["None", "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+            wind_list = ["None", "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW",
+                         "NW", "NNW"]
             for i in range(0, len(wind_list)):
                 if d3[1] == wind_list[i]:
                     self.wind_com.set_active(i)
@@ -240,7 +242,8 @@ class EditDialog(Gtk.Dialog):
         self.airp_sbtn.set_value(float(d5[0]))
         self.visi_sbtn.set_value(float(data[7]))
         clou_list1 = ["Sunny", "Mostly Sunny", "Partly Cloudy", "Mostly Cloudy", "Cloudy"]
-        clou_list2 = ["None", "Unknown", "Cirrus", "Cirrocumulus", "Cirrostratus", "Cumulonimbus", "Altocumulus", "Altostratus", 
+        clou_list2 = ["None", "Unknown", "Cirrus", "Cirrocumulus", "Cirrostratus", "Cumulonimbus", "Altocumulus",
+                      "Altostratus",
                       "Stratus", "Cumulus", "Stratocumulus"]
         d81, d82 = datasets.split_list3([data[8]])
         d82 = datasets.strip_items(d82, ["(", ")"])
@@ -253,11 +256,11 @@ class EditDialog(Gtk.Dialog):
                 self.clou_com2.set_active(i)
                 break
         self.note_buffer.set_text(data[9])
-        
+
         # Connect 'Enter' key to the OK button.
-        ok_btn = self.get_widget_for_response(response_id = Gtk.ResponseType.OK)
+        ok_btn = self.get_widget_for_response(response_id=Gtk.ResponseType.OK)
         ok_btn.set_can_default(True)
         ok_btn.grab_default()
-        
+
         # Show the dialog.
         self.show_all()
